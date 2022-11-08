ca9ddbc498b32767697a6d6cdc8aa59e41ddb1f2
Marek Piotrowski <marekpiotrowski2@icpt.pl>
2022-11-08 10:55:55 +0100

    Clean code

--- BetterConsoleApp/ConsoleUI/Program.cs
+++ BetterConsoleApp/ConsoleUI/Program.cs
@@ -1,10 +1,9 @@
-﻿namespace ConsoleUI
+﻿namespace ConsoleUI;
+
+public class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.WriteLine("Hello, World!");
     }
 }
